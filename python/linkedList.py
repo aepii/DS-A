63db77c376f527bd75ec95c7bb6f4ccb70d07d01
sijmario <simentalma@yahoo.com>
2024-03-13 13:44:37 -0500

    Unfinished linked list

--- python/linkedList.py
+++ python/linkedList.py
@@ -15,6 +15,7 @@ class LinkedList:
         if self.head == self.tail:
             self.tail = new_node
         self.head.next = new_node
+        self.length += 1
 
     def append(self, value):
         new_node = Node(value)
@@ -22,8 +23,22 @@ class LinkedList:
             self.head.next = new_node
         self.tail.next = new_node
         self.tail = new_node
+        self.length += 1
         
     def insert(self, value, index):
         if index == 0:
             self.prepend(self, value)
-        elif index >= 
+        elif index >= self.length:
+            self.append(self, value)
+        else:
+            new_node = Node(value)
+            curr = self.head.next
+            i = 0
+            while curr:
+                if i == index:
+                    temp = curr
+                    curr = new_node
+                    curr.next = temp
+                i += 1
+                curr = curr.next
+               
