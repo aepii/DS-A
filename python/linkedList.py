aef618eaa42d14ed79674016a2c2ad6127896e52
sijmario <simentalma@yahoo.com>
2024-03-13 14:24:51 -0500

    Progress on linked list

--- python/linkedList.py
+++ python/linkedList.py
@@ -3,6 +3,7 @@ class Node:
         self.value = value
         self.next = None
 
+# Linked list implementation with dummy nodes and length.
 class LinkedList:
     def __init__(self):
         dummy_node = Node(-1)
@@ -14,6 +15,7 @@ class LinkedList:
         new_node = Node(value)
         if self.head == self.tail:
             self.tail = new_node
+        new_node.next = self.head.next
         self.head.next = new_node
         self.length += 1
 
@@ -21,24 +23,57 @@ class LinkedList:
         new_node = Node(value)
         if self.head == self.tail:
             self.head.next = new_node
-        self.tail.next = new_node
+        else:
+            self.tail.next = new_node
         self.tail = new_node
         self.length += 1
         
     def insert(self, value, index):
         if index == 0:
-            self.prepend(self, value)
+            self.prepend(value)
         elif index >= self.length:
-            self.append(self, value)
+            self.append(value)
         else:
             new_node = Node(value)
+            prev = self.head
             curr = self.head.next
             i = 0
             while curr:
                 if i == index:
-                    temp = curr
-                    curr = new_node
-                    curr.next = temp
+                    new_node.next = curr
+                    prev.next = new_node
+                    self.length += 1
+                    break
+                i += 1
+                prev = curr
+                curr = curr.next
+
+    def delete(self)
+
+    def search(self, value):
+        if self.head == self.tail:
+            return False
+        else:
+            curr = self.head.next
+            while curr:
+                if curr.value == value:
+                    return True
+                curr = curr.next
+            return False
+        
+    def get(self, index):
+        if self.head == self.tail:
+            raise ValueError("The list is empty.")
+        elif index >= self.length or index < 0:
+            raise IndexError("Index out of bounds.")
+        else:
+            curr = self.head.next
+            i = 0
+            while curr:
+                if i == index:
+                    return curr.value
                 i += 1
                 curr = curr.next
-               
+
+    def size(self):
+        return self.length
