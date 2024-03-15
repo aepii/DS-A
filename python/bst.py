efbca34d608948aeacf5f35567a3ecf0889e62ac
aepii <simentalma@yahoo.com>
2024-03-14 19:37:19 -0500

    BST Progress.

--- python/bst.py
+++ python/bst.py
@@ -9,16 +9,17 @@ class BST:
         self.root = None
     
     def insert(self, value):
-        self._recursive_insert(self.root, value)
+        self.root = self._recursive_insert(self.root, value)
 
     def _recursive_insert(self, node, value):
         if node is None:
             node = BinaryNode(value)
-            return
+            return node
         elif value > node.value:
-            self._recursive_insert(node.right, value)
+            node.right = self._recursive_insert(node.right, value)
         elif value < node.value:
-            self._recursive_insert(node.left, value)
+            node.left = self._recursive_insert(node.left, value)
+        return node
 
     def search(self, value):
         pass
